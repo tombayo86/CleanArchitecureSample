61aa075ec9d74c1612c42c61e004fd6936014dc4
Tom <bajorek.tomasz@gmail.com>
2019-09-02 11:10:06 +0200

    Project setup

---
+++ Support/Support/Support.h
@@ -0,0 +1,19 @@
+//
+//  Support.h
+//  Support
+//
+//  Created by Tom on 9/2/19.
+//  Copyright © 2019 Tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Support.
+FOUNDATION_EXPORT double SupportVersionNumber;
+
+//! Project version string for Support.
+FOUNDATION_EXPORT const unsigned char SupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Support/PublicHeader.h>
+
+
